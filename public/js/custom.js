e2767934238a098235795d7c716fd3f2eb2c78e8
dawsonrichey <dawson.richey@ttu.edu>
2021-03-15 00:20:38 -0500

    called

--- public/js/custom.js
+++ public/js/custom.js
@@ -5,8 +5,6 @@ let url = 'http://localhost:4000/contact';
 fetch(url)
 .then(res => res.json())
 .then((out) => {
-//   var myJSON2 = JSON.parse(out);
-// document.getElementById("demo2").innerHTML = myJSON2;
 
 for (x in out) {
     document.getElementById("demo").innerHTML += 
@@ -16,6 +14,7 @@ for (x in out) {
     out[x]['phone'] + '<br/>';
   }
 })
+
 .catch(err => { throw err });
 
 
