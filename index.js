cd1146a0d7a2965487b5608495e40d358023ce5e
dawsonrichey <dawson.richey@ttu.edu>
2021-03-14 21:23:40 -0500

    set up schema

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import express from 'express';
 import routes from './src/routes/crmRoutes';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 4000;
 
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb', {
+    useNewParser: true,
+    useUnifiedTopology: true
+});
+
+// bodyparser connection
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
 routes(app);
+
 app.get('/', (req, res) =>
     res.send(`Node and express server running on port ${PORT}`)
 );
